682cef68092f4f0907554386fd9f4993caac6fff
bmcgeestudent <83534601+bmcgeestudent@users.noreply.github.com>
2021-05-03 13:29:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
-class config:
-    BOT_TOKEN = "1792903332:AAEgg0rWPa5TO2U9L0czpk4vh_ECZW6NCgc"
-    APP_ID = "5999444"
-    API_HASH = "51b58a93d88a55565ff048c7a143eb4e"
+import  os
+
+BOT_TOKEN = "1792903332:AAEgg0rWPa5TO2U9L0czpk4vh_ECZW6NCgc"
+APP_ID = "5999444"
+API_HASH = "51b58a93d88a55565ff048c7a143eb4e"
 
 youtube_next_fetch = 0  # time in minute
 
