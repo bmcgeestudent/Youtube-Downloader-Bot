0b0f02bba55ed1a8396c35a5931fa70e5d306165
bmcgeestudent <83534601+bmcgeestudent@users.noreply.github.com>
2021-05-03 13:49:43 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,8 +4,6 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 @Client.on_message(Filters.command(["start"]), group=-2)
 async def start(client, message):
     # return
-   
-    ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n I'm Gopal's YouTube Uploader"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
